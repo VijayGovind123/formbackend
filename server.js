3077941a7d7abc10c01f19af46bf900724fff8fa
Ande Kishore <120158003+VijayGovind123@users.noreply.github.com>
2023-09-02 13:40:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -185,14 +185,19 @@ function generateCustomLetter(reqBody) {
   // Create the custom letter content in a formal manner
   doc.fontSize(16).text(`Statement of Purpose for ${name}`);
   doc.moveDown(0.5);
-
+ doc.moveDown(1);
  doc.fontSize(16).text(`Hi ${name},`);
+  doc.moveDown(1);
   doc.fontSize(12).text("Thanks for using our free SOP drafting service! Your SOP is attached below.");
+  doc.moveDown(1);
   doc.fontSize(12).text("If you would like further help as follows:");
+  doc.moveDown(1);
   doc.fontSize(12).text("1. Get a complete statement of purpose framed/reviewed by our experts - Buy it here:", {
     link: "https://effizient-immigration-inc.square.site/product/sop/9?cp=true&sa=true&sbp=false&q=false",
   });
+  doc.moveDown(1);
   doc.fontSize(12).text("2. Get your visa application reviewed before submission to IRCC");
+  doc.moveDown(1);
   doc.fontSize(12).text("Feel free to contact us!");
   doc.fontSize(12).text("226-774-9168");
   doc.fontSize(12).text("info@effizient.ca");
@@ -200,25 +205,40 @@ function generateCustomLetter(reqBody) {
   doc.fontSize(12).text("We will get you going with your visa application in no time. This will all be remote, which means you won’t have any hassle at all!");
   doc.fontSize(12).text("Best Regards,");
   doc.fontSize(12).text("Team Effizient");
+  doc.moveDown(1);
+  doc.moveDown(1);
 
   doc.fontSize(12).text(`From\n${name}\n(Address)\n${email}`);
   doc.fontSize(12).text("To");
   doc.fontSize(12).text("Visa Officer");
   doc.fontSize(12).text("High Commission of Canada");
   doc.fontSize(12).text("Subject: Statement of Purpose for studying in Canada");
+  doc.moveDown(1);
+  doc.moveDown(1);
 
   // Customize the body of the letter with the provided form data
   doc.fontSize(12).text(`Dear Sir/Madam,`);
+  doc.moveDown(1);
   doc.fontSize(12).text(`I would like to take this opportunity to introduce myself as ${name}, a ${age}-year-old individual from ${country}. I have recently completed my ${degree} from ${university}, and I am now seeking to further my education in Canada.`);
+  doc.moveDown(1);
   doc.fontSize(12).text(`I have always been passionate about ${branch}, and I have a strong desire to pursue a career in this field. This passion and love for ${branch} led me to pursue a ${degree} in ${branch}, where I gained a solid foundation in the subject.`);
+  doc.moveDown(1);
   doc.fontSize(12).text(`After extensive research, I have found that the program of study in ${branchto} at ${instituteto} is the perfect fit for me. The program offers a comprehensive and interdisciplinary approach to ${branchto}, which will provide me with a well-rounded understanding of the subject.`);
+  doc.moveDown(1);
   doc.fontSize(12).text(`Additionally, the faculty at ${instituteto} are highly experienced and skilled, and I believe that studying and working with them will greatly enhance my academic pursuit.`);
+  doc.moveDown(1);
   doc.fontSize(12).text(`Furthermore, studying in Canada offers numerous advantages. Canada is known for its excellent education system, which is ranked among the best in the world. The country is also known for its safe and peaceful environment, as well as its superb healthcare facilities. Studying in Canada will not only provide me with a high-quality education, but it will also expose me to diverse cultures and improve my communication skills.`);
+  doc.moveDown(1);
   doc.fontSize(12).text(`My future goal is to become a successful and dynamic professional in the field of ${branchto}. I believe that by pursuing my studies in Canada, I will gain the necessary knowledge and skills to achieve this goal. Upon completion of my program, I plan to return to ${country} and contribute to the growth and development of the ${branchto} industry in my home country. I am confident that the education and experience I gain in Canada will make me stand out among my peers and enable me to make a significant impact in the industry.`);
+  doc.moveDown(1);
   doc.fontSize(12).text(`In terms of my academic background, I have successfully completed my ${degree} from ${university}, which has provided me with a strong foundation in ${branch}. Additionally, I have achieved an IELTS score of ${feegic} in listening, ${fee} in reading, ${gic} in writing, and ${goals} in speaking, demonstrating my proficiency in the English language.`);
+  doc.moveDown(1);
   doc.fontSize(12).text(`In terms of finances, I am fortunate to have the support of my family who will be funding my education in Canada. I have enough finances to cover my tuition fees for the first year and living expenses.`);
+  doc.moveDown(1);
   doc.fontSize(12).text(`Dear Madam/Sir, if I am granted the opportunity to study in Canada, I assure you that I will abide by all the rules and regulations set by the Canadian government, the local authorities, and the educational institution.`);
+  doc.moveDown(1);
   doc.fontSize(12).text(`I kindly request you to process my visa application as soon as possible. I am grateful for your time and consideration.`);
+  doc.moveDown(1);
   doc.fontSize(12).text(`Sincerely,`);
   doc.fontSize(12).text(name);
   doc.end();
