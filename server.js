b5e2f5537b43ec8e9c9056edf093664f49983b3a
Ande Kishore <120158003+VijayGovind123@users.noreply.github.com>
2023-09-02 13:26:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -186,119 +186,41 @@ function generateCustomLetter(reqBody) {
   doc.fontSize(16).text(`Statement of Purpose for ${name}`);
   doc.moveDown(0.5);
 
-  doc
-    .fontSize(12)
-    .text(`Date: ${new Date().toDateString()}`, { align: "right" });
-
-  doc.moveDown(1);
-
-  doc.fontSize(12).text(`Recipient's Address:`, { underline: true });
-  doc.fontSize(12).text(`[Recipient's Address Line 1]`);
-  doc.fontSize(12).text(`[Recipient's Address Line 2]`);
-  doc.fontSize(12).text(`[Recipient's Address Line 3]`);
-  doc.fontSize(12).text(`[Recipient's City, State, ZIP Code]`);
-  doc.moveDown(0.5);
-
-  doc.fontSize(14).text("Dear Sir/Madam,", { align: "left" });
-  doc.moveDown(0.5);
-
-  doc
-    .fontSize(12)
-    .text(
-      `I am writing to express my interest in pursuing a ${degree} program at ${university}.`,
-      { align: "left" }
-    );
-  doc.moveDown(0.2);
-
-  doc
-    .fontSize(12)
-    .text(
-      `My name is ${name} and I am ${age} years old. I hold a Bachelor's degree in ${branch} from ${university}.`,
-      { align: "left" }
-    );
-  doc.moveDown(0.2);
-
-  doc
-    .fontSize(12)
-    .text(
-      `I have ${workexp} years of work experience in the field of ${branch}.`,
-      { align: "left" }
-    );
-  doc.moveDown(0.2);
-
-  doc
-    .fontSize(12)
-    .text(
-      `My educational journey has instilled in me a passion for learning and a drive to excel in ${degree}.`,
-      { align: "left" }
-    );
-  doc.moveDown(0.2);
-
-  doc
-    .fontSize(12)
-    .text(`I am particularly interested in ${instituteto} in ${branchto}.`, {
-      align: "left",
-    });
-  doc.moveDown(0.2);
-
-  doc
-    .fontSize(12)
-    .text(
-      `I aspire to contribute to the field of ${branchto} and believe that ${university} is the ideal place to nurture my skills and knowledge.`,
-      { align: "left" }
-    );
-  doc.moveDown(0.2);
-
-  doc.fontSize(12).text(`In terms of my future goals, I plan to ${goals}.`, {
-    align: "left",
+ doc.fontSize(16).text(`Hi ${name},`);
+  doc.fontSize(12).text("Thanks for using our free SOP drafting service! Your SOP is attached below.");
+  doc.fontSize(12).text("If you would like further help as follows:");
+  doc.fontSize(12).text("1. Get a complete statement of purpose framed/reviewed by our experts - Buy it here:", {
+    link: "https://effizient-immigration-inc.square.site/product/sop/9?cp=true&sa=true&sbp=false&q=false",
   });
-  doc.moveDown(0.2);
+  doc.fontSize(12).text("2. Get your visa application reviewed before submission to IRCC");
+  doc.fontSize(12).text("Feel free to contact us!");
+  doc.fontSize(12).text("226-774-9168");
+  doc.fontSize(12).text("info@effizient.ca");
+  doc.fontSize(12).text("www.effizieint.ca");
+  doc.fontSize(12).text("We will get you going with your visa application in no time. This will all be remote, which means you won’t have any hassle at all!");
+  doc.fontSize(12).text("Best Regards,");
+  doc.fontSize(12).text("Team Effizient");
 
-  doc
-    .fontSize(12)
-    .text(
-      `Regarding the payment of tuition fees, I have the following response to the payment question: ${payQ}.`,
-      { align: "left" }
-    );
-  doc.moveDown(0.2);
+  doc.fontSize(12).text(`From\n${name}\n(Address)\n${email}`);
+  doc.fontSize(12).text("To");
+  doc.fontSize(12).text("Visa Officer");
+  doc.fontSize(12).text("High Commission of Canada");
+  doc.fontSize(12).text("Subject: Statement of Purpose for studying in Canada");
 
-  doc
-    .fontSize(12)
-    .text(
-      `The fee structure for the ${degree} program at ${university} is as follows: ${fee}.`,
-      { align: "left" }
-    );
-  doc.moveDown(0.2);
-
-  doc
-    .fontSize(12)
-    .text(
-      `I have arranged for the Guaranteed Investment Certificate (GIC) as a part of the financial requirements.`,
-      { align: "left" }
-    );
-  doc.moveDown(0.2);
-
-  doc
-    .fontSize(12)
-    .text(`The GIC amount for ${gic} covers my living expenses and more.`, {
-      align: "left",
-    });
-  doc.moveDown(0.2);
-
-  doc
-    .fontSize(12)
-    .text(
-      `I have attached the fee receipt and GIC details with this application.`,
-      { align: "left" }
-    );
-
-  doc.moveDown(1);
-
-  doc.fontSize(12).text("Sincerely,", { align: "right" });
-
-  doc.fontSize(12).text(`${name}`, { align: "right" });
-  doc.fontSize(12).text(`Email: ${email}`, { align: "right" });
-
+  // Customize the body of the letter with the provided form data
+  doc.fontSize(12).text(`Dear Sir/Madam,`);
+  doc.fontSize(12).text(`I would like to take this opportunity to introduce myself as ${name}, a ${age}-year-old individual from ${country}. I have recently completed my ${degree} from ${university}, and I am now seeking to further my education in Canada.`);
+  doc.fontSize(12).text(`I have always been passionate about ${branch}, and I have a strong desire to pursue a career in this field. This passion and love for ${branch} led me to pursue a ${degree} in ${branch}, where I gained a solid foundation in the subject.`);
+  doc.fontSize(12).text(`After extensive research, I have found that the program of study in ${branchto} at ${instituteto} is the perfect fit for me. The program offers a comprehensive and interdisciplinary approach to ${branchto}, which will provide me with a well-rounded understanding of the subject.`);
+  doc.fontSize(12).text(`Additionally, the faculty at ${instituteto} are highly experienced and skilled, and I believe that studying and working with them will greatly enhance my academic pursuit.`);
+  doc.fontSize(12).text(`Furthermore, studying in Canada offers numerous advantages. Canada is known for its excellent education system, which is ranked among the best in the world. The country is also known for its safe and peaceful environment, as well as its superb healthcare facilities. Studying in Canada will not only provide me with a high-quality education, but it will also expose me to diverse cultures and improve my communication skills.`);
+  doc.fontSize(12).text(`My future goal is to become a successful and dynamic professional in the field of ${branchto}. I believe that by pursuing my studies in Canada, I will gain the necessary knowledge and skills to achieve this goal. Upon completion of my program, I plan to return to ${country} and contribute to the growth and development of the ${branchto} industry in my home country. I am confident that the education and experience I gain in Canada will make me stand out among my peers and enable me to make a significant impact in the industry.`);
+  doc.fontSize(12).text(`In terms of my academic background, I have successfully completed my ${degree} from ${university}, which has provided me with a strong foundation in ${branch}. Additionally, I have achieved an IELTS score of ${feegic} in listening, ${fee} in reading, ${gic} in writing, and ${goals} in speaking, demonstrating my proficiency in the English language.`);
+  doc.fontSize(12).text(`In terms of finances, I am fortunate to have the support of my family who will be funding my education in Canada. I have enough finances to cover my tuition fees for the first year and living expenses.`);
+  doc.fontSize(12).text(`Dear Madam/Sir, if I am granted the opportunity to study in Canada, I assure you that I will abide by all the rules and regulations set by the Canadian government, the local authorities, and the educational institution.`);
+  doc.fontSize(12).text(`I kindly request you to process my visa application as soon as possible. I am grateful for your time and consideration.`);
+  doc.fontSize(12).text(`Sincerely,`);
+  doc.fontSize(12).text(name);
   doc.end();
 
   return pdfFilePath;
